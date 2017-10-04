b8378677c8efc3b59e43676a9cf53bc14ba99ba0
Aram Anderson <aram.j.anderson@gmail.com>
2017-10-04 09:08:39 -0600

    Fix factories and tests for created_at and updated_at

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :customer do
     first_name "First"
     last_name "Last"
+    created_at "2012-03-27 14:54:09 UTC"
+    updated_at "2012-03-27 14:54:09 UTC"
   end
 end
