824afb3de8e3edfe14c97dd827f632911f0a58b3
Boots2291 <buteramv@gmail.com>
2017-10-05 18:32:04 -0600

    Add two more tests

--- spec/requests/api/v1/business_logic_spec.rb
+++ spec/requests/api/v1/business_logic_spec.rb
@@ -7,7 +7,7 @@ def test_prep
   merchant_1 = create(:merchant)
   merchant_2 = create(:merchant)
   merchant_3 = create(:merchant)
-  invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
+  invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, created_at: "2014-02-27 14:54:09 UTC")
   invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
   invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
   invoice_4 = create(:invoice, customer_id: customer_2.id, merchant_id: merchant_1.id)
@@ -20,7 +20,7 @@ def test_prep
   item_5 = create(:item, merchant_id: merchant_2.id)
   item_6 = create(:item, merchant_id: merchant_3.id)
   item_7 = create(:item, merchant_id: merchant_3.id)
-  invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, quantity: 5)
+  invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, quantity: 10)
   invoice_item_2 = create(:invoice_item, invoice_id: invoice_2.id)
   invoice_item_3 = create(:invoice_item, invoice_id: invoice_3.id)
   invoice_item_4 = create(:invoice_item, invoice_id: invoice_4.id)
@@ -45,6 +45,32 @@ context "All merchants" do
 
       expect(response).to be_success
       expect(merchant.first["id"]).to eq(1)
+    end
+  end
+
+  describe "GET /api/v1/merchants/most_items?quantity=x" do
+    it "returns the top x merchants ranked by total number of items sold" do
+      test_prep
+
+      get '/api/v1/merchants/most_items?quantity=1'
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant.first["id"]).to eq(1)
+    end
+  end
+
+  describe "GET /api/v1/merchants/revenue?date=x" do
+    it "returns the total revenue for date x across all merchants" do
+      test_prep
+
+      get '/api/v1/merchants/revenue?date=2014-02-27+14:54:09+UTC'
+
+      revenue = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(revenue["total_revenue"]).to eq("100.0")
     end
   end
 end
